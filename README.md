39394e5013bdd89a6efceacc0b5de7a46dba7ebb
Shahadat Hossain <shahadath@hsenidmobile.com>
2019-12-10 13:18:03 +0600

    complete crud example

---
+++ README.md
@@ -0,0 +1,17 @@
+# CRUD-React App
+
+## Pre-requisites
+
+* Install *yarn*
+
+```bash
+npm install yarn --global
+```
+
+## Tasks
+
+### Start DEV server
+
+```bash
+yarn start
+```
---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import "./resources/styles/common.css"
+import "bootstrap/dist/css/bootstrap.min.css"
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from "./App"
+
+ReactDOM.render(<App />, document.getElementById('root'));
